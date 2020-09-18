6fb6c1688abddaf8b08000b3621642a11befa363
Bob <949222328@qq.com>
2020-09-18 17:33:54 +0800

    我的主页

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ let jh = document.querySelector('.jh');
 let qg = document.querySelector('.qg');
 // 2.注册点击事件
 jh.onclick = function() {
-    // 3.判断
+    // 3. 判断
     let key = prompt('江汉学校学部主任：');
     if (key == '李元荣') {
         window.location.href = './jh.html';
