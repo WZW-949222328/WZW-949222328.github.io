c2bf418da7deb13b219da3b7654f60e447555cf1
Bob <949222328@qq.com>
2020-09-18 17:35:04 +0800

    我的主页

--- js/index.js
+++ js/index.js
@@ -2,15 +2,15 @@
 let jh = document.querySelector('.jh');
 let qg = document.querySelector('.qg');
 // 2.注册点击事件
-jh.onclick = function() {
-    // 3. 判断
-    let key = prompt('江汉学校学部主任：');
-    if (key == '李元荣') {
-        window.location.href = './jh.html';
-    } else {
-        alert('输入错误，请重试');
-    }
-};
+// jh.onclick = function() {
+//     // 3. 判断
+//     let key = prompt('江汉学校学部主任：');
+//     if (key == '李元荣') {
+//         window.location.href = './jh.html';
+//     } else {
+//         alert('输入错误，请重试');
+//     }
+// };
 qg.onclick = function() {
     // 3.判断
     let key = prompt('1509寝室长');
