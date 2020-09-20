f73577ec379adc3713b7675b69f29eb17932f842
Bob <949222328@qq.com>
2020-09-20 09:05:18 +0800

    我的主页

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 window.onload = function() {
     // 1.获取元素
-    let jh = document.querySelector('.jh');
-    let qg = document.querySelector('.qg');
+    let one = document.querySelector('.one');
+    let two = document.querySelector('.two');
     // 2. 注册点击事件
-    jh.onclick = function() {
+    one.onclick = function() {
         // 3. 判断
         let key = prompt('江汉学校学部主任：');
         if (key == '李元荣') {
@@ -12,7 +12,7 @@ window.onload = function() {
             alert('输入错误，请重试');
         }
     };
-    qg.onclick = function() {
+    two.onclick = function() {
         // 3.判断
         let key = prompt('1509寝室长');
         if (key == '王哲武') {
