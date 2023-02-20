0f0beabae9ec2e40236c3817087b49d681839c9e
zhewu <zhewu>
2023-02-20 18:15:05 +0800

    预彩更新

--- js/index.js
+++ js/index.js
@@ -1,32 +1,42 @@
 window.onload = function () {
   // 1.获取元素
-  let one = document.querySelector('.one')
-  let two = document.querySelector('.two')
+  // let one = document.querySelector('.one')
+  // let two = document.querySelector('.two')
   let three = document.querySelector('.three')
-  // 2. 注册点击事件
-  one.onclick = function () {
-    // 3. 判断
-    let key = prompt('江汉学校高中部主任：')
-    if (key == '李元荣') {
-      window.location.href = './jh.html'
-    } else {
-      alert('输入错误，请重试')
-    }
-  }
-  two.onclick = function () {
-    // 3.判断
-    let key = prompt('1509寝室长')
-    if (key == '王哲武') {
-      window.location.href = './1509.html'
-    } else {
-      alert('输入错误，请重试')
-    }
-  }
+  let four = document.querySelector('.four')
+  // // 2. 注册点击事件
+  // one.onclick = function () {
+  //   // 3. 判断
+  //   let key = prompt('江汉学校高中部主任：')
+  //   if (key == '李元荣') {
+  //     window.location.href = './jh.html'
+  //   } else {
+  //     alert('输入错误，请重试')
+  //   }
+  // }
+  // two.onclick = function () {
+  //   // 3.判断
+  //   let key = prompt('1509寝室长')
+  //   if (key == '王哲武') {
+  //     window.location.href = './1509.html'
+  //   } else {
+  //     alert('输入错误，请重试')
+  //   }
+  // }
   three.onclick = function () {
     // 3.判断
     // let key = prompt('1509寝室长');
     // if (key == '王哲武') {
     window.location.href = './dzcq.html'
+    // } else {
+    // alert('输入错误，请重试');
+    // }
+  }
+  four.onclick = function () {
+    // 3.判断
+    // let key = prompt('1509寝室长');
+    // if (key == '王哲武') {
+    window.location.href = './tcdlt.html'
     // } else {
     // alert('输入错误，请重试');
     // }
